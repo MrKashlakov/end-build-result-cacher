3a927cabe4b43d3605abb0a1b5e63fa7726e3679
Ilya Kashlakov <i.kashlakov@yandex.ru>
2016-04-02 16:20:11 +0300

    Add async require

--- get-build-result.js
+++ get-build-result.js
@@ -1,7 +1,24 @@
+var Vow = require('vow');
 var vowFs = require('vow-fs');
+var requireOrEval = 
+
+var _requireFormCache = function (result) {
+	var cacheModule = new Module.constructor();
+	cacheModule._compile(result);
+	return cacheModule.exports;
+}
 
 var asyncRequre = function (node, fileName) {
-	
+	var defer = Vow.defer();
+	var cache = this.node.getNodeCache();
+	var key = ['build-result', fileName].join('-');
+	var result = cache.get(key);
+
+	if (!result) {
+		return requireOrEval(fileName);
+	}
+	defer.resolve(_requireFormCache(result));
+	return defer.promise();
 };
 
 var readFile = function (node, fileName, encoding) {
@@ -11,7 +28,7 @@ var readFile = function (node, fileName, encoding) {
 
 	if (!result) {
 		console.log('use fs', fileName);
-		return vowFs.read(fileName, 'UTF-8');
+		return vowFs.read(fileName, encoding || 'UTF-8');
 	}
 	defer.resolve(result);
 	return defer.promise();
---
+++ index.js
@@ -0,0 +1,4 @@
+module.eaxports = {
+	saveBuidResult: require('./save-build-result')
+	getBuildResult: require('./get-build-result');
+}
